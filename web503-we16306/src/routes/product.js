a7c1726c135f97165241e47547501c675ad0acc3
VuongWeb <vuonglvph15121@fpt.edu.vn>
2022-04-05 08:29:34 +0700

    first commit

---
+++ web503-we16306/src/routes/product.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { create, get, list, remove, update } from '../controllers/product';
+import { userById } from '../controllers/user';
+import { checkAuth, isAdmin, isAuth, requireSignin } from '../middleware/checkAuth';
+
+const router = Router();
+
+router.get("/products", list);
+router.get("/products/:id", get);
+router.post('/products/:userId', requireSignin, isAuth, isAdmin, create);
+router.delete("/products/:id", remove);
+router.put("/products/:id", update);
+
+router.param("userId", userById)
+
+export default router
